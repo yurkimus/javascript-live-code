d3d966c05531dabfccb6a1e23cf091415030556e
Iurii <51003204+yurkimus@users.noreply.github.com>
2025-08-06 18:28:03 +0200

    Update phase.js

--- phase.js
+++ phase.js
@@ -1,7 +1,22 @@
+/**
+ * @typedef {any} Phase
+ */
+
+/**
+ * @satisfies {Phase}
+ */
 let phase = null
 
+/**
+ * @satisfies {(value: Phase) => Phase}
+ */
 let setPhase = (value) => (phase = value)
 
+/**
+ * @param {Phase} phase
+ *
+ * @returns {string}
+ */
 let getPhaseMessage = phase => {
   switch (phase) {
     case true:
@@ -11,6 +26,6 @@ let getPhaseMessage = phase => {
       return 'Data is [Loaded]'
 
     default:
-      return `Message for state "${state}" is not defined`
+      return `No message provided for phase '${Phase}'.`
   }
 }
